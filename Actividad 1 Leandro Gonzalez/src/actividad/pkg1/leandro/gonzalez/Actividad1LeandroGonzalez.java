69a365c220e9283ce86e569d397227404e16826b
lagonzalezcaniza-stack <lagonzalezcaniza@gmail.com>
2026-03-10 15:56:27 -0300

    Actividad de variables

---
+++ Actividad 1 Leandro Gonzalez/src/actividad/pkg1/leandro/gonzalez/Actividad1LeandroGonzalez.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package actividad.pkg1.leandro.gonzalez;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Actividad1LeandroGonzalez {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int p=10;
+        System.out.println(p);
+        int q=2;
+        System.out.println(q);
+        float x=2.8f;
+        System.out.println(x);
+         float y=5.7f;
+        System.out.println(y);
+         float z=9.2f;
+        System.out.println(z);
+        char a ='L';
+        System.out.println(a);
+        char b ='E';
+        System.out.println(b);
+        char c ='A';
+        System.out.println(c);
+        double raiz1=8.7;
+        System.out.println(raiz1);
+        double raiz2=3.6;
+        System.out.println(raiz2);
+        long contador=12000;
+        System.out.println(contador);
+        short indicador = 11;
+        System.out.println(indicador);
+        int indice=4;
+        System.out.println(indice);
+        
+        
+        
+        
+    }
+    
+}
