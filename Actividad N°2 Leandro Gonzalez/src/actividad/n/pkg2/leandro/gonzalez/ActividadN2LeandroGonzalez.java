7302cceb15d482a620133489aeffe58cf235328d
lagonzalezcaniza.stack <lagonzalezcaniza>
2026-03-17 15:52:12 -0300

    ejercicios if else

--- Actividad N°2 Leandro Gonzalez/src/actividad/n/pkg2/leandro/gonzalez/ActividadN2LeandroGonzalez.java
+++ Actividad N°2 Leandro Gonzalez/src/actividad/n/pkg2/leandro/gonzalez/ActividadN2LeandroGonzalez.java
@@ -7,7 +7,7 @@ package actividad.n.pkg2.leandro.gonzalez;
 import java.util.Scanner;
 
 /**
-onic
+
  * @author Alumno
  */
 public class ActividadN2LeandroGonzalez {
@@ -15,35 +15,277 @@ public class ActividadN2LeandroGonzalez {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    /**public static void main(String[] args) {
        
       // 1. Creamos un objeto Scanner para leer lo que escribes en el teclado
-        Scanner teclado = new Scanner(System.in);
+          Scanner teclado = new Scanner(System.in);
 
-        // 2. Pedimos los dos números
+         2. Pedimos los dos números
         System.out.print("Introduce el primer número: ");
         int num1 = teclado.nextInt();
 
         System.out.print("Introduce el segundo número: ");
         int num2 = teclado.nextInt();
 
-        // 3. Estructura if-el3se para comparar
+         3. Estructura if-el3se para comparar
         if (num1 == num2) {
-            System.out.println("Los números " + num1 + " y " + num2 + " son IGUALES.");
+            //System.out.println("Los números " + num1 + " y " + num2 + " son IGUALES.");
         } else {
-            System.out.println("Los números " + num1 + " y " + num2 + " son DIFERENTES.");
+            //System.out.println("Los números " + num1 + " y " + num2 + " son DIFERENTES.");
         }
 
         // Cerramos el scannerj
-        teclado.close();
+        //teclado.close();
+        */
     }
 
-}
+    /**
 
-    
+     * @param args the command line arguments
 
-    
+     */
+/**
+   public static void main(String[] args) { 
 
+      
+
+
+       Scanner teclado = new Scanner(System.in);
+
+
+        System.out.print("Introduce una nota entre 0 y 10: ");
+
+        double nota = teclado.nextDouble();
+
+
+        if (nota < 0 || nota > 10) {
+
+            System.out.println("La nota introducida no es válida.");
+
+        } else if (nota < 5) {
+
+            System.out.println("Insuficiente");
+
+        } else if (nota < 6) {
+
+            System.out.println("Suficiente");
+
+        } else if (nota < 7) {
+
+            System.out.println("Bien");
+
+        } else if (nota < 9) {
+
+            System.out.println("Notable");
+
+        } else {
+
+            System.out.println("Sobresaliente");
+
+        }
+
+
+        teclado.close();
+
+    }
+
+*/
 
    
+
+      //  Scanner teclado = new Scanner(System.in);
+
+
+        //System.out.print("Introduce el primer número: ");
+
+       // int num1 = teclado.nextInt();
+
+
+     //   System.out.print("Introduce el segundo número: ");
+
+       // int num2 = teclado.nextInt();
+
+
+       // System.out.print("Introduce el tercer número: ");
+
+       // int num3 = teclado.nextInt();
+
+
+      //  int mayor = num1;
+
+
+       // if (num2 > mayor) {
+
+       //     mayor = num2;
+
+      //  }
+
+
+     //   if (num3 > mayor) {
+
+         //   mayor = num3;
+
+      //  }
+
+
+     //   System.out.println("El número mayor es: " + mayor);
+
+
+       // teclado.close();
+
+//    }
+
+//}
+
+
+     //   Scanner teclado = new Scanner(System.in);
+
+
+    //    System.out.print("Introduce un año: ");
+
+      //  int anio = teclado.nextInt();
+
+
+      //  if ((anio % 4 == 0 && anio % 100 != 0) || (anio % 400 == 0)) {
+
+         //   System.out.println("El año es bisiesto.");
+
+   //     } else {
+
+         //   System.out.println("El año no es bisiesto.");
+
+      //  }
+
+
+      //  teclado.close();
+
+   // }
+
+//}
+
+   
+
+
+      //  Scanner teclado = new Scanner(System.in);
+
+
+      //  System.out.print("Introduce un número entero: ");
+
+      //  int numero = teclado.nextInt();
+
+
+     //   if (numero % 3 == 0 && numero % 5 == 0) {
+
+            //System.out.println("El número es múltiplo de 3 y de 5.");
+
+      //  } else {
+
+            //System.out.println("El número NO es múltiplo de 3 y de 5.");
+
+      //  }
+
+
+       // teclado.close();
+
+   // }
+
+//}
+
+        //Scanner teclado = new Scanner(System.in);
+
+
+       // System.out.print("Introduce el primer número: ");
+
+       // int num1 = teclado.nextInt();
+
+
+       // System.out.print("Introduce el segundo número: ");
+
+       // int num2 = teclado.nextInt();
+
+
+      //  int ultima1 = num1 % 10;
+
+       // int ultima2 = num2 % 10;
+
+
+    //    if (ultima1 == ultima2) {
+
+          //  System.out.println("Los dos números terminan con la misma cifra.");
+
+       // } else {
+
+           // System.out.println("Los números terminan con cifras diferentes.");
+
+        //}
+
+
+       // teclado.close();
+
+    //}
+
+//}
+
+        
+
+            //  System.out.println("introducir numero1");
+
+            //   int num1 = teclado.nextInt();
+
+               //  System.out.println("introducir numero2");
+
+               //int num2 = teclado.nextInt();
+
+              
+
+              // if (num1<num2){
+
+                //System.out.println("el numero mayor es:" +num2 );
+
+
+                 // else if (num1> num2){
+
+                // System.out.println("el numero mayor es:" +num1 );
+
+
+    // else {
+
+                //System.out.println("los dos numeros son iguales:" );
+
+
+
+
+    
+
+                
+
+       
+
+        //System.out.println("introducir numero1");
+
+         //int num1= teclado.nextInt();
+
+         
+
+        //System.out.println("introducir numero2");
+
+         //int num2= teclado.nextInt();
+
+        //if(num1 == num2 ) {
+
+        //System.out.println("son iguales ");
+
+    //} else{
+
+        //System.out.println("no son iguales");
+
+            //}
+
+      //teclado.close();
+
+
+
+    
+
+    
 
