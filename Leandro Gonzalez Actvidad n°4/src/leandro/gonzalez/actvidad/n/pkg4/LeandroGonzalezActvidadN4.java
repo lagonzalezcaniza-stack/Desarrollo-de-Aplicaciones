7ea66f4b89d0c3a3a45f4d9e6186dd484a223887
lagonzalezcaniza-stack <lagonzalezcaniza@gmail.com>
2026-04-13 17:36:32 -0300

    Acts 4

--- Leandro Gonzalez Actvidad n°4/src/leandro/gonzalez/actvidad/n/pkg4/LeandroGonzalezActvidadN4.java
+++ Leandro Gonzalez Actvidad n°4/src/leandro/gonzalez/actvidad/n/pkg4/LeandroGonzalezActvidadN4.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package leandro.gonzalez.actvidad.n.pkg4;
-
+import java.util.Scanner;
 /**
  *
  * @author Alumno
@@ -13,8 +13,85 @@ public class LeandroGonzalezActvidadN4 {
     /**
      * @param args the command line arguments
      */
+    /*Primer Ejercicios 1a
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        int n= 1;
+        while(n<=100){
+            System.out.println(n);
+            n++;
+        }
+              
+                
     }
     
+}
+*/
+    /*Primer Ejercicio 1b
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+   for(int b = 1;b<=100;b++){
+    System.out.println(b);
+   }
+    }
+}
+   */
+    /*Primer Ejercicio 1c
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int b = 1;
+    do{
+        System.out.println(b);
+        b++;
+    }while (b <=100);
+        
+    }
+            
+    }
+      */  
+    /*
+     public static void main(String[] args) {
+    Scanner teclado = new Scanner(System.in);
+    int N = teclado.nextInt();
+    int N1= 1;
+        while(N1 <= N){
+            System.out.println(N1);
+            N1++;
+        }
+     }
+        }
+    */
+    /*
+     public static void main(String[] args) {
+    Scanner teclado = new Scanner(System.in);
+    int N = teclado.nextInt();
+    int N1= 1;
+        do{
+            System.out.println(N);
+            N--;
+        } while (N1<= N);     
+     }
+     }
+     */
+  /*
+ public static void main(String[] args) {
+    Scanner teclado = new Scanner(System.in);
+    int N = teclado.nextInt();
+    int N1= 1;
+        while(N >= N1){
+            System.out.println(N);
+            N--;
+        }
+     }
+        }
+*/
+   
+    public static void main(String[] args) {
+     Scanner teclado = new Scanner(System.in);
+     Scanner sc = new Scanner(System.in);
+     int N = teclado.nextInt();
+     for(int N1 = 1; N1<=N; N--){
+      System.out.println(N);
+   }
+    }
 }
